d60619a956183c44cee4039c074966faacdda1e4
enrique.gomez <enrigomur@gmail.com>
2025-02-19 13:19:15 +0100

    Subida inicial del proyecto

---
+++ Codigo/Prueba.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Análisis Inmobiliario", layout="wide")
+
+st.title(" Análisis del Mercado Inmobiliario")
+st.sidebar.header("Navegación")
+st.sidebar.markdown("Selecciona una sección para explorar los datos.")
+
+st.write("Bienvenido a la aplicación de análisis de pisos.")
