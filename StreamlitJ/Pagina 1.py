19e66292137af0a8924d053e4d5979a0e8480ce6
Jorge Gazulla <jgazulla@MacBook-Pro-de-Jorge.local>
2025-02-21 13:14:12 +0100

    Actualización en el código de scrapeo. Mejora en datos obtenidos para analisis N/A->Si. Pruebas con Streamlit.

---
+++ StreamlitJ/Pagina 1.py
@@ -0,0 +1,70 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import requests
+
+def main():
+    # Title
+    st.title("ANALISIS INMOBILIARIO CON PISOS.COM")
+
+    # Text
+    st.text("Bienvenidos a nuestra página de análisis inmobiliario con Pisos.com.")
+    st.text("Esta página será la muestra de nuestro PFB")
+    name = "Jorge Gazulla"
+
+    # # Header
+    st.header("Header1")
+
+    # # Subheader
+    st.subheader("Subheader1")
+
+    # # Markdown
+    st.markdown("# This is markdown.")
+
+    # # Display Colored Text/Boostraps Alert
+    st.success("success")
+    st.warning("warning")
+    st.info("info")
+    st.error("error")
+    st.exception("exception.")
+
+    # .write()
+    st.write("Normal Text.")
+    st.write("## This is a markdown text")
+    st.write(1 + 2)
+    st.dataframe(dir(st))
+
+    # # Help
+    st.help(range)
+
+    # Display Data
+    df = pd.read_csv(filepath_or_buffer = "sources/alquileres_scrap_completo.csv", sep = ";")
+
+    # Dinamic Data
+    st.dataframe(df)
+    st.write(df)
+
+    # Static Table
+    st.table(df)
+
+    # Adding Color
+    st.dataframe(df.select_dtypes(include=np.number).style.highlight_max(axis = 0))
+
+    # Display JSON
+    #endpoint = "https://api.frankfurter.app/latest"
+    #response = requests.get(url=endpoint)
+    #st.json(response.json())
+
+    # # Display Code
+    # code = """
+    # def func():
+    #     return x**2
+    # """
+    # st.code(body = code, language = "python")
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
