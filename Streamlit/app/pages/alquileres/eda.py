bb849fe07e7cdebec6ae31b33102e9567f30b027
Jorge Gazulla <jgazulla@MacBook-Pro-de-Jorge.local>
2025-03-03 19:34:39 +0100

    Subidos algunos graficos faltantes(mapa y outliers) a la seccion eda dentro de ambas paginas.

--- Streamlit/app/pages/alquileres/eda.py
+++ Streamlit/app/pages/alquileres/eda.py
@@ -2,30 +2,138 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import plotly.express as px
+import folium
+import geopandas as gpd
+import json
 
-df = pd.read_csv("data/alquileres_completo_limpio.csv", low_memory=False)
-df = df.dropna(subset=['precio'])
-# Identificar y eliminar outliers
-Q1 = df['precio'].quantile(0.05)
-Q3 = df['precio'].quantile(0.95)
-IQR = Q3 - Q1
-filtro = (df['precio'] >= (Q1 - 1.5 * IQR)) & (df['precio'] <= (Q3 + 1.5 * IQR))
-df = df[filtro]
-df = df[df['precio'] >= 10]
+#df = pd.read_csv("data/alquileres_completo_limpio.csv", low_memory=False)
+#df = df.dropna(subset=['precio'])
+## Identificar y eliminar outliers
+#Q1 = df['precio'].quantile(0.05)
+#Q3 = df['precio'].quantile(0.95)
+#IQR = Q3 - Q1
+#filtro = (df['precio'] >= (Q1 - 1.5 * IQR)) & (df['precio'] <= (Q3 + 1.5 * IQR))
+#df = df[filtro]
+#df = df[df['precio'] >= 10]
 
 # Eliminar filas donde el valor de la columna agencia sea "Agencia no disponible"
-df = df[df['agencia'] != "Agencia no disponible"]
+#df = df[df['agencia'] != "Agencia no disponible"]
 
-def vista_graficos():
-    st.sidebar.title("Navegación")
-    if st.sidebar.button("Página Principal"):
-        st.session_state.page = "primera_pagina"
-    if st.sidebar.button("Vista Detallada"):
-        st.session_state.page = "vista_detallada"
-
+def eda_page():
     st.title("Vista de Gráficos")
     st.write("Aquí puedes ver la presentación de los datos obtenidos y como se relacionan entre sí.")
-    # Aquí se insertaran tambien filtros, mapa etc..
+    #MAPA COROPLÉTICO
+    def mapa_coropletico(file_path):
+        st.markdown("## Mapa Coroplético de Pisos/Casas en alquiler por Provincia")
+        # Cargamos el archivo GeoJSON de las provincias de España
+        geojson_path = "data/spanish_provinces.geojson"
+        with open(geojson_path, 'r') as f:
+            geojson_data = json.load(f)
+        gdf_spain = gpd.GeoDataFrame.from_features(geojson_data["features"])
+        # Cargamos el archivo CSV
+        df = pd.read_csv(file_path, low_memory=False)
+        # Agrupamos por provincia y contamos el número de pisos/casas
+        df_grouped = df.groupby('provincia').size().reset_index(name='counts')
+        # Creamos un diccionario de mapeo para asegurarnos de que coincidan los nombres de las provincias en ambos archivos
+        mapping = {
+            'A Coruña': 'A Coruña',
+            'Alava': 'Araba',
+            'Albacete': 'Albacete',
+            'Alicante': 'Alacant',
+            'Almeria': 'Almería',
+            'Asturias': 'Asturias',
+            'Avila': 'Ávila',
+            'Badajoz': 'Badajoz',
+            'Barcelona': 'Barcelona',
+            'Burgos': 'Burgos',
+            'Caceres': 'Cáceres',
+            'Cadiz': 'Cádiz',
+            'Cantabria': 'Cantabria',
+            'Castellon': 'Castelló',
+            'Ciudad Real': 'Ciudad Real',
+            'Cordoba': 'Córdoba',
+            'Cuenca': 'Cuenca',
+            'Girona': 'Girona',
+            'Granada': 'Granada',
+            'Guadalajara': 'Guadalajara',
+            'Guipuzcoa': 'Gipuzcoa',
+            'Huelva': 'Huelva',
+            'Huesca': 'Huesca',
+            'Islas Baleares': 'Illes Balears',
+            'Jaen': 'Jaén',
+            'La Rioja': 'La Rioja',
+            'Las Palmas': 'Las Palmas',
+            'Leon': 'León',
+            'Lleida': 'Lleida',
+            'Lugo': 'Lugo',
+            'Madrid': 'Madrid',
+            'Malaga': 'Málaga',
+            'Melilla': 'Melilla',
+            'Murcia': 'Murcia',
+            'Navarra': 'Navarra',
+            'Ourense': 'Ourense',
+            'Palencia': 'Palencia',
+            'Pontevedra': 'Pontevedra',
+            'Salamanca': 'Salamanca',
+            'Santa Cruz de Tenerife': 'Santa Cruz de Tenerife',
+            'Segovia': 'Segovia',
+            'Sevilla': 'Sevilla',
+            'Soria': 'Soria',
+            'Tarragona': 'Tarragona',
+            'Teruel': 'Teruel',
+            'Toledo': 'Toledo',
+            'Valencia': 'València',
+            'Valladolid': 'Valladolid',
+            'Vizcaya': 'Bizkaia',
+            'Zamora': 'Zamora',
+            'Zaragoza': 'Zaragoza'
+        }
+        # Normalizar los nombres de las provincias en ambos DataFrames y aplicar el mapeo
+        gdf_spain['provincia_normalized'] = gdf_spain['provincia'].map(mapping)
+        df_grouped['provincia_normalized'] = df_grouped['provincia'].map(mapping)
+        # Unimos los datos con el GeoDataFrame
+        gdf_spain = gdf_spain.merge(df_grouped, left_on='provincia_normalized', right_on='provincia_normalized', how='left')
+        # Creamos el mapa coroplético con Folium
+        m = folium.Map(location=[40.416775, -3.703790], zoom_start=6)
+
+        folium.Choropleth(
+            geo_data=geojson_data,
+            name='choropleth',
+            data=df_grouped,
+            columns=['provincia_normalized', 'counts'],
+            key_on='feature.properties.provincia',
+            fill_color='Oranges',
+            fill_opacity=0.9,
+            line_opacity=0.2,
+            legend_name='Cantidad de pisos/casas en alquiler por provincia'
+        ).add_to(m)
+
+        folium.LayerControl().add_to(m)
+        # Renderizamos el mapa directamente en Streamlit
+        st.components.v1.html(m._repr_html_(), height=600)
+    mapa_coropletico("data/alquileres_completo_limpio.csv")
+
+    #ANALISIS DE OULIERS EN PRECIO
+    def analizar_outliers_precio(file_path):
+        st.markdown("## Análisis de outliers en columna precio de los alquileres")
+        # Cargamos el archivo CSV
+        df = pd.read_csv(file_path, low_memory=False)
+        # Transformamos la columna precio a su valor logarítmico
+        df['log_precio'] = np.log(df['precio'])
+        # Calculamos el Z-Score e identificamos outliers
+        df['z_score'] = (df['log_precio'] - df['log_precio'].mean()) / df['log_precio'].std()
+        df['outlier'] = df['z_score'].apply(lambda x: 'Outlier' if np.abs(x) > 3 else 'Normal')
+        # Creamos el histograma con Plotly
+        fig = px.histogram(df, x='log_precio', color='outlier', title='Histograma de Precios Logarítmicos con Outliers',
+                           color_discrete_map={'Outlier': 'red', 'Normal': 'green'})
+        fig.update_layout(
+            xaxis_title='Precio Logarítmico',
+            yaxis_title='Frecuencia'
+        )
+        st.plotly_chart(fig)
+    analizar_outliers_precio("data/alquileres_completo_limpio.csv")
 
     ##FRAN
     def scatter_segmentado(df, x, y, segmentar_por):
@@ -45,13 +153,13 @@ def vista_graficos():
         # Mostrar gráfico en Streamlit
         st.pyplot(fig)
 
-    scatter_segmentado(df, 'superficie_construida', 'precio', 'CCAA')
+    scatter_segmentado(df, 'superficie_construida', 'precio', 'comunidad_autonoma')
 
-    st.write("### Conclusiones del análisis de precio vs. superficie construida por CCAA")
+    st.write("### Conclusiones del análisis de precio vs. superficie construida por comunidad autonoma")
     st.write("Concentración en rangos bajos: La mayoría de las propiedades tienen superficies menores a 200 m² y precios inferiores a 10,000€")
     st.write("Valores atípicos: Existen algunas propiedades con precios muy elevados (hasta 80,000€) y grandes superficies (+600 m²)")
     st.write("Tendencia positiva: En general, el precio tiende a aumentar con la superficie construida, aunque con gran dispersión")
-    st.write("Diferencias entre CCAA: Regiones como Madrid, Cataluña e Islas Baleares presentan precios más elevados en comparación con otras comunidades.")
+    st.write("Diferencias entre comunidadautonoma: Regiones como Madrid, Cataluña e Islas Baleares presentan precios más elevados en comparación con otras comunidades.")
     st.write("Mercado heterogéneo: Aunque la relación precio-superficie es clara, otros factores como ubicación")
 
     ##ENRIQUE
@@ -175,3 +283,7 @@ def vista_graficos():
 
     # Mostrar gráfico en Streamlit
     st.pyplot(fig)
+
+
+if __name__ == "__main__":
+    eda_page()
--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,20 +1,22 @@
 import streamlit as st
-from app.pages import home, eda, database, about_us
-from app.pages.compra_venta import comparador as cv_comparador, clustering as cv_clustering, classification as cv_classification, regression as cv_regression
-from app.pages.alquileres import comparador as alq_comparador, clustering as alq_clustering, classification as alq_classification, regression as alq_regression
+from app.pages import home, database, about_us
+from app.pages.compra_venta import eda as cv_eda, comparador as cv_comparador, clustering as cv_clustering, classification as cv_classification, regression as cv_regression
+from app.pages.alquileres import eda as alq_eda, comparador as alq_comparador, clustering as alq_clustering, classification as alq_classification, regression as alq_regression
 
 def main():
     st.set_page_config(page_title="Análisis de Datos de Pisos.com", layout="wide")
 
     # Sidebar
     st.sidebar.title("Navegación")
-    main_page = st.sidebar.selectbox("Selecciona una sección principal", ["Página Principal", "Compra/Venta", "Alquileres", "Exploratory Data Analysis", "Base de Datos", "About Us"], index=0)
+    main_page = st.sidebar.selectbox("Selecciona una sección principal", ["Página Principal", "Compra/Venta", "Alquileres", "Base de Datos", "Sobre nosotros"], index=0)
 
     if main_page == "Página Principal":
         home.main()
     elif main_page == "Compra/Venta":
-        sub_page = st.sidebar.selectbox("Selecciona una subpágina", ["Comparador", "Clustering", "Clasificación", "Regresión"])
-        if sub_page == "Comparador":
+        sub_page = st.sidebar.selectbox("Selecciona una subpágina", ["Análisis de datos", "Comparador", "Clustering", "Clasificación", "Regresión"])
+        if sub_page == "Análisis de datos":
+            cv_eda.eda_page()
+        elif sub_page == "Comparador":
             cv_comparador.comparador_page()
         elif sub_page == "Clustering":
             cv_clustering.main()
@@ -23,8 +25,10 @@ def main():
         elif sub_page == "Regresión":
             cv_regression.main()
     elif main_page == "Alquileres":
-        sub_page = st.sidebar.selectbox("Selecciona una subpágina", ["Comparador", "Clustering", "Clasificación", "Regresión"])
-        if sub_page == "Comparador":
+        sub_page = st.sidebar.selectbox("Selecciona una subpágina", ["Análisis de datos", "Comparador", "Clustering", "Clasificación", "Regresión"])
+        if sub_page == "Análisis de datos":
+            alq_eda.eda_page()
+        elif sub_page == "Comparador":
             alq_comparador.comparador_page()
         elif sub_page == "Clustering":
             alq_clustering.main()
@@ -32,8 +36,6 @@ def main():
             alq_classification.main()
         elif sub_page == "Regresión":
             alq_regression.main()
-    elif main_page == "Exploratory Data Analysis":
-        eda.main()
     elif main_page == "Base de Datos":
         database.main()
     elif main_page == "About Us":
